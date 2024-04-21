ff60e8e2cce9e65de1caca8ad8ad0c67f4e57290
kentheckel <kent.heckel@gmail.com>
2024-04-21 13:18:35 -0400

    Initial Commit

---
+++ RedouteWatchV2 Watch App/RedouteWatchV2App.swift
@@ -0,0 +1,17 @@
+//
+//  RedouteWatchV2App.swift
+//  RedouteWatchV2 Watch App
+//
+//  Created by Kennett Heckel on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RedouteWatchV2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
